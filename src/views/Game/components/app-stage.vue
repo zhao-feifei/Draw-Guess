956396248c523c749101a2c82895a962e46f219d
zhao-feifei <398983914@qq.com>
2021-08-18 20:53:05 +0800

    绘图插件配置完成

--- src/views/Game/components/app-stage.vue
+++ src/views/Game/components/app-stage.vue
@@ -1,6 +1,15 @@
 <template>
   <el-card ref="wrapper" :body-style="{ padding: 0 }">
-    <div style="height: 600px;">画布</div>
+    <v-stage
+      :config="stageConfig"
+      @mousedown="mousedownHandler"
+      @mouseup="mouseupHandler"
+      @mousemove="mousemoveHandler"
+    >
+      <v-layer>
+        <v-line v-for="(line, index) in lines" :key="index" :config="line" />
+      </v-layer>
+    </v-stage>
   </el-card>
 </template>
 
@@ -8,20 +17,44 @@
 export default {
   data() {
     return {
-
-    }
-  },
-
-  computed: {
-
+      // 画布配置
+      stageConfig: {
+        width: 800,
+        height: 700
+      },
+      // 绘画状态
+      painting: false,
+      lines: [
+        { stroke: "#df4b26", strokeWidth: 5, points: [100, 100, 100, 400] },
+        { stroke: "#ff00ff", strokeWidth: 5, points: [100, 100, 300, 300] }
+      ]
+    };
   },
 
   mounted() {
-
+    // 将画布宽度设置与容器同宽
+    this.stageConfig.width = this.$refs.wrapper.$el.offsetWidth;
   },
 
   methods: {
+    // 鼠标按下
+    mousedownHandler(e) {
+      console.log("鼠标按下了");
+      this.painting = true;
+    },
 
+    // 鼠标拖动
+    mousemoveHandler(e) {
+      if (this.painting) {
+        console.log("鼠标拖动了, 绘画状态");
+      }
+    },
+
+    // 鼠标释放
+    mouseupHandler() {
+      console.log("鼠标释放了");
+      this.painting = false;
+    }
   }
-}
+};
 </script>
